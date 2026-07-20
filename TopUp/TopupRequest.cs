34b1230ea2a18f4c96b634c689e2880aa1154109
unknown <arezoo.sdghi@gmail.com>
2026-07-20 17:52:20 +0330

    feat(topup): add request models.

---
+++ TopUp/TopupRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TopUp
+{
+    public class TopupRequest
+    {
+        public int RetryCount { get; set; }
+        public required string Mobile { get; set; }
+        public required decimal Amount { get; set; }
+        public required Guid TransactionId { get; set; }
+    }
+}
