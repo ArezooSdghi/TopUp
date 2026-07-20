c0083bc5c6de74ad8d02b79c4eac539dd88c4381
unknown <arezoo.sdghi@gmail.com>
2026-07-20 18:42:44 +0330

    feat(switch): implement switch service interface.

---
+++ TopUp/SwitchService.cs
@@ -0,0 +1,18 @@
+﻿
+namespace TopUp
+{
+    public class SwitchService : ISwitchService
+    {
+        public Task AdviceAsync(Guid transactionId)
+        {
+            Console.WriteLine($"Advice sent for {transactionId}");
+            return Task.CompletedTask;
+        }
+
+        public Task ReverseAsync(Guid transactionId)
+        {
+            Console.WriteLine($"Reverse sent for {transactionId}");
+            return Task.CompletedTask;
+        }
+    }
+}
