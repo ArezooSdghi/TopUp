92d68a2b33d1a8abea203eef8f2bbb5464e9da46
unknown <arezoo.sdghi@gmail.com>
2026-07-20 21:18:28 +0330

    feat(transaction): add dispatcher for transaction handlers.

---
+++ TopUp/TransactionDispatcher.cs
@@ -0,0 +1,19 @@
+﻿namespace TopUp
+{
+    public class TransactionDispatcher
+    {
+        private readonly IEnumerable<ITransactionHandler> _handlers;
+
+        public TransactionDispatcher(IEnumerable<ITransactionHandler> handlers)
+        {
+            _handlers = handlers;
+        }
+
+        public async Task DispatchAsync(Transaction transaction)
+        {
+            var handler = _handlers.FirstOrDefault(x => x.Type == transaction.Type);
+            if (handler is null) throw new Exception("Handler not found");
+            await handler.HandleAsync(transaction);
+        }
+    }
+}
